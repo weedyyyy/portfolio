5bc9cf06dcc6c4970629681c48800bf3716c5d6d
weedyyyy <89921111+weedyyyy@users.noreply.github.com>
2024-06-25 22:02:36 +0100

    first comit

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "chihab's Portfolio",
+	description: "Modern & Minimal chihab bouzar Portfolio",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html
+			lang='en'
+			suppressHydrationWarning>
+			<head>
+				<link
+					rel='icon'
+					href='favicon.ico'
+					sizes='any'
+				/>
+			</head>
+			<body className={inter.className}>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='dark'
+					enableSystem
+					disableTransitionOnChange>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
+}
---
+++ data/index.ts
@@ -0,0 +1,215 @@
+export const navItems = [
+	{ name: "About", link: "#about" },
+	{ name: "Projects", link: "#projects" },
+	{ name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+	{
+		id: 1,
+		title: "I prioritize client collaboration, fostering open communication ",
+		description: "",
+		className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+		imgClassName: "w-full h-full",
+		titleClassName: "justify-end",
+		img: "/b1.svg",
+		spareImg: "",
+	},
+	{
+		id: 2,
+		title: "I'm very flexible with time zone communications",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+		imgClassName: "",
+		titleClassName: "justify-start",
+		img: "",
+		spareImg: "",
+	},
+	{
+		id: 3,
+		title: "My tech stack",
+		description: "I constantly try to improve",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+		imgClassName: "",
+		titleClassName: "justify-center",
+		img: "",
+		spareImg: "",
+	},
+	{
+		id: 4,
+		title: "Tech enthusiast with a passion for development.",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+		imgClassName: "",
+		titleClassName: "justify-start",
+		img: "/grid.svg",
+		spareImg: "/b4.svg",
+	},
+
+	{
+		id: 5,
+		title: "Currently building APP for my Start Up ",
+		description: "The Inside Scoop",
+		className: "md:col-span-3 md:row-span-2",
+		imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+		titleClassName: "justify-center md:justify-start lg:justify-center",
+		img: "/b5.svg",
+		spareImg: "/grid.svg",
+	},
+	{
+		id: 6,
+		title: "Do you want to start a project together?",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+		imgClassName: "",
+		titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+		img: "",
+		spareImg: "",
+	},
+];
+
+export const projects = [
+	{
+		id: 1,
+		title: "Brainwave - Modern UI/UX website",
+		des: "developed using React.js and Tailwind CSS, exemplifies modern UI/UX principles,seamless animations",
+		img: "/brainwave.png",
+		iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+		link: "https://brain-uiux-ae6okzwe3-chihab-bouzars-projects.vercel.app/",
+	},
+	{
+		id: 2,
+		title: "E-Commerce headless with WIX",
+		des: "Developed with Next.js and Wix Headless",
+		img: "/ecom.png",
+		iconLists: ["/next.svg", "/tail.svg", "/ts.svg"],
+		link: "/ui.yoom.com",
+	},
+	{
+		id: 3,
+		title: "Youtube Clone",
+		des: "Modern YouTube Clone Application in React JS with Material UI 5",
+		img: "/youtubeclone.png",
+		iconLists: ["/re.svg", "/s.svg", "/three.svg", "/c.svg"],
+		link: "https://famous-cucurucho-79fbf0.netlify.app/",
+	},
+	{
+		id: 4,
+		title: "Animated Apple Iphone 3D Website",
+		des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+		img: "/p4.svg",
+		iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+		link: "https://iphone-3d-clone.vercel.app/",
+	},
+];
+
+export const testimonials = [
+	{
+		quote: "Collaborating with chihab was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. chihab's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, chihab is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote: "Collaborating with chihab was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. chihab's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, chihab is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote: "Collaborating with chihab was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. chihab's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, chihab is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote: "Collaborating with chihab was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. chihab's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, chihab is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote: "Collaborating with chihab was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. chihab's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, chihab is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+];
+
+export const companies = [
+	{
+		id: 1,
+		name: "Faculty of New Technology in Computer Science and Communications",
+		img: "/ntic.png",
+		nameImg: "/Faculty of New Technology in Computer Science and Communications.svg",
+	},
+	{
+		id: 2,
+		name: "appwrite",
+		img: "/app.svg",
+		nameImg: "/appName.svg",
+	},
+	{
+		id: 3,
+		name: "HOSTINGER",
+		img: "/host.svg",
+		nameImg: "/hostName.svg",
+	},
+	{
+		id: 4,
+		name: "stream",
+		img: "/s.svg",
+		nameImg: "/streamName.svg",
+	},
+	{
+		id: 5,
+		name: "docker.",
+		img: "/dock.svg",
+		nameImg: "/dockerName.svg",
+	},
+];
+
+export const workExperience = [
+	{
+		id: 1,
+		title: "Frontend Engineer Intern",
+		desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+		className: "md:col-span-2",
+		thumbnail: "/exp1.svg",
+	},
+	{
+		id: 2,
+		title: "Web Scrapingt",
+		desc: "Created a database of potential clients by web scraping relevant websites, contributing to enhanced marketing strategies.",
+		className: "md:col-span-2",
+		thumbnail: "/exp2.svg", // Suggestion for a new icon
+	},
+	{
+		id: 3, // Assuming this is the second entry
+		title: "Information Technology",
+		desc: "Configured routers and switches, designed and implemented VLANs, and troubleshot network issues using Packet Tracer simulationsP.",
+		className: "md:col-span-2",
+		thumbnail: "/exp3.svg", // Suggestion for a network-related icon
+	},
+
+	{
+		id: 4,
+		title: "Lead Frontend Developer",
+		desc: "Developed and maintained user-facing features using modern frontend technologies.",
+		className: "md:col-span-2",
+		thumbnail: "/exp4.svg",
+	},
+];
+
+export const socialMedia = [
+	{
+		id: 1,
+		img: "/git.svg",
+		link: "https://github.com/weedyyyy",
+	},
+	{
+		id: 2,
+		img: "/twit.svg",
+		link: "",
+	},
+	{
+		id: 3,
+		img: "/link.svg",
+		link: "https://www.linkedin.com/in/chihab-bouzar-essaidi-0730a7200/",
+	},
+];
